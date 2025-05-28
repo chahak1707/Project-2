0e4c21a16e6fa2ca0e0580534bdae952465a070c
chahak1707 <pahujamanni1978@gmail.com>
2025-05-28 13:49:30 +0530

    Project 2

---
+++ main.py
@@ -0,0 +1,10 @@
+Birthday_01 = "Rahul-14/07/2015"
+Birthday_02 = "Rajesh-08/07/2015"
+Birthday_03 = "Ram-18/07/2015"
+Birthday_04 = "Ramesh-04/07/2015"
+Birthday_05 = "Rohan-22/07/2015"
+print(Birthday_01)
+print(Birthday_02)
+print(Birthday_03)
+print(Birthday_04)
+print(Birthday_05)
